32c802529248edd3a7404beeddc54acf771d3f70
david@directip.com.br <david@direcitp.com.br>
2016-06-14 21:51:39 +0000

    Removendo projetos de teste

--- module/NovaCli/src/NovaCli/V1/Rest/NovaCli/NovaCliCollection.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace NovaCli\V1\Rest\NovaCli;
-
-use Zend\Paginator\Paginator;
-
-class NovaCliCollection extends Paginator
-{
-}
--- module/NovaCli/src/NovaCli/V1/Rest/NovaCli/NovaCliEntity.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace NovaCli\V1\Rest\NovaCli;
-
-use ArrayObject;
-
-class NovaCliEntity extends ArrayObject
-{
-}
